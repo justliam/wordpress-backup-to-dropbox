4efc61a958c8bee3f0fb5bd6d38b8e10bff4e11c
michaeldewildt <mikey@99designs.com>
2011-12-21 19:52:47 +1100

    Updated release script

--- release.sh
+++ release.sh
@@ -2,11 +2,13 @@
 TRUNK='../../svn/wordpress-backup-to-dropbox/trunk/'
 ARCHIVE='../../../releases/wordpress-backup-to-dropbox.zip'
 
+cp *.txt $TRUNK
+cp *.png $TRUNK
 cp *.php $TRUNK
-cp -R PEAR_Includes $TRUNK
-cp -R Languages $TRUNK
-cp -R JQueryFileTree $TRUNK
-cp -R Dropbox_API $TRUNK
+cp -r PEAR_Includes $TRUNK
+cp -r Languages $TRUNK
+cp -r JQueryFileTree $TRUNK
+cp -r Dropbox_API $TRUNK
 
 cd $TRUNK
-zip $ARCHIVE *
+zip -r $ARCHIVE * -x '*/.svn/*'
