b9ea23aa7a3d6d69fc16c3f5054d7607a91bc223
Michael De Wildt <michael.dewildt@99designs.com>
2012-07-17 19:21:26 +1000

    Pre construct the backup here

--- Views/wp-backup-to-dropbox-monitor.php
+++ Views/wp-backup-to-dropbox-monitor.php
@@ -19,13 +19,14 @@
  *          Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA.
  */
 $config = WP_Backup_Config::construct();
+$backup = new WP_Backup();
 
 if (array_key_exists('stop_backup', $_POST)) {
 	check_admin_referer('backup_to_dropbox_monitor_stop');
-	WP_Backup::construct()->stop();
+	$backup->stop();
 } else if (array_key_exists('start_backup', $_POST)) {
 	check_admin_referer('backup_to_dropbox_monitor_stop');
-	WP_Backup::construct()->backup_now();
+	$backup->backup_now();
 	$started = true;
 }
 
