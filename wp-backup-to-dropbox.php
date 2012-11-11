826375c956ce0c63f9097a6a10176b4b7358cfba
Michael De Wildt <michael.dewildt@99designs.com>
2012-11-11 17:16:09 +1100

    Version bump

--- wp-backup-to-dropbox.php
+++ wp-backup-to-dropbox.php
@@ -21,9 +21,7 @@ License: Copyright 2011  Michael De Wildt  (email : michael.dewildt@gmail.com)
 		along with this program; if not, write to the Free Software
 		Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-define('USE_BUNDLED_PEAR', true);
-define('BACKUP_TO_DROPBOX_VERSION', '1.3');
-define('BACKUP_TO_DROPBOX_ERROR_TIMEOUT', 5); //seconds
+define('BACKUP_TO_DROPBOX_VERSION', '1.4');
 define('EXTENSIONS_DIR', implode(array(WP_CONTENT_DIR, 'plugins', 'wordpress-backup-to-dropbox', 'Extensions'), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR);
 
 require_once('Dropbox/Dropbox/API.php');
