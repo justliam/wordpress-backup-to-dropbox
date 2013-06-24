a97dbc6e7d9e2212cec3a9bfae72a3b7bda69c62
Michael De Wildt <michael.dewildt@99designs.com>
2013-06-24 20:11:00 +1000

    Include prefix in error matcher

--- wp-backup-to-dropbox.php
+++ wp-backup-to-dropbox.php
@@ -299,7 +299,7 @@ function wpb2d_install() {
 	global $EZSQL_ERROR;
 	if ($EZSQL_ERROR) {
 		foreach ($EZSQL_ERROR as $error) {
-			if (preg_match('/^CREATE TABLE wpb2d_/', $error['query']))
+			if (preg_match("/^CREATE TABLE {$wpdb->prefix}wpb2d_/", $error['query']))
 				$errors[] = $error['error_str'];
 		}
 
