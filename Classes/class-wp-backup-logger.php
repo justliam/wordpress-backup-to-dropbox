e029e674da0c46cbc2861e254d1fcb0f8f877e7c
Michael De Wildt <michael.dewildt@gmail.com>
2013-08-26 15:17:26 +1000

    Find log file with secret

--- Classes/class-wp-backup-logger.php
+++ Classes/class-wp-backup-logger.php
@@ -18,53 +18,56 @@
  */
 class WP_Backup_Logger {
 
-	const LOGFILE = 'wpb2d-backup-log%s.txt%s';
+	const LOGFILE = 'wpb2d-backup-log.txt';
+
+	private $logFile = null;
 
 	public function log($msg, $files = null) {
 		$fh = fopen(self::get_log_file(), 'a');
 		$log = sprintf("%s: %s", date('H:i:s', strtotime(current_time('mysql'))), $msg) . "\n";
 
-		if (!empty($files))
+		if (!empty($files)) {
 			$log .= "Uploaded Files:" . json_encode($files) . "\n";
+		}
 
-		if (@fwrite($fh, $log) === false || @fclose($fh) === false)
+		if (@fwrite($fh, $log) === false || @fclose($fh) === false) {
 			throw new Exception('Error writing to log file.');
+		}
 	}
 
 	public function get_log() {
 		$file = self::get_log_file();
-		if (!file_exists($file))
+		if (!file_exists($file)) {
 			return false;
+		}
 
 		$contents = trim(file_get_contents($file));
-		if (strlen($contents) < 1)
+		if (strlen($contents) < 1) {
 			return false;
+		}
 
 		return explode("\n", $contents);
 	}
 
 	public function delete_log() {
+		$this->logFile = null;
 		@unlink(self::get_log_file());
 	}
 
 	public function get_log_file() {
-		WP_Backup::create_dump_dir();
+		if (!$this->logFile) {
+			WP_Backup::create_dump_dir();
 
-		$path = WP_Backup_Registry::config()->get_backup_dir() . DIRECTORY_SEPARATOR . self::LOGFILE;
+			$path = WP_Backup_Registry::config()->get_backup_dir() . DIRECTORY_SEPARATOR . self::LOGFILE;
 
-		$secret = WP_Backup_Registry::get_secret($file);
-		$file = sprintf($path, '', $secret);
-
-		$fh = @fopen($file, 'a');
-		if ($fh === false) {
-
-			$file = sprintf($path, '-v1', $secret);
-			$fh = @fopen($file, 'a');
-			if ($fh === false)
-				throw new Exception('Error opening log file.');
+			$files = glob($path . '.*');
+			if (isset($files[0])) {
+				$this->logFile = $files[0];
+			} else {
+				$this->logFile = $path . '.' . WP_Backup_Registry::get_secret($file);
+			}
 		}
-		fclose($fh);
 
-		return $file;
+		return $this->logFile;
 	}
 }
